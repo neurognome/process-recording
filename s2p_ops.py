5f8056172f19f9991060565cbc2268e94490b58a
Kevin Sit <kevinkamansit@gmail.com>
2023-02-09 11:27:33 -0800

    switch environments

--- s2p_ops.py
+++ s2p_ops.py
@@ -14,7 +14,7 @@ default_ops = {
     'keep_movie_raw': True, # must be true for 2 step reg
     'two_step_registration': True,
     'nimg_init': 800, # subsampled frames for finding reference image
-    'batch_size': 500, #2000, # number of frames per batch, default=500
+    'batch_size': 2000, #2000, # number of frames per batch, default=500
     'align_by_chan': 1, # 1-based, use 2 for tdT
     
     # non rigid registration settings
@@ -56,7 +56,7 @@ gcamp8m  = {
     'keep_movie_raw': False, # must be true for 2 step reg
     'two_step_registration': False,
     'nimg_init': 800, # subsampled frames for finding reference image
-    'batch_size': 500, #2000, # number of frames per batch, default=500
+    'batch_size': 2000, #2000, # number of frames per batch, default=500
     'align_by_chan': 1, # 1-based, use 2 for tdT
     
     # non rigid registration settings
@@ -94,6 +94,8 @@ red  = {
     'save_NWB': False,
     'tau': 1,
     'combined': False,
+
+    # cellpose
     'anatomical_only': 1,
 
     # registration
@@ -101,8 +103,8 @@ red  = {
     'keep_movie_raw': False, # must be true for 2 step reg
     'two_step_registration': False,
     'nimg_init': 800, # subsampled frames for finding reference image
-    'batch_size': 500, #2000, # number of frames per batch, default=500
-    'align_by_chan': 1, # 1-based, use 2 for tdT
+    'batch_size': 2000, #2000, # number of frames per batch, default=500
+    'align_by_chan': 2, # 1-based, use 2 for tdT
     
     # non rigid registration settings
     'nonrigid': False, # whether to use nonrigid registration
@@ -113,15 +115,8 @@ red  = {
     'sparse_mode': False,
     'max_iterations': 50, # usualy stops at threshold scaling, default 20
     'high_pass': 100,  # running mean subtraction with window of size 'high_pass' (use low values for 1P), default 100
-    # 'classifier_path': 'c:/users/will/code/suite2p/suite2p/classifiers/classifier_8m.npy',
     
-    # deconvolution settings
-    'baseline': 'maximin',  # baselining mode (can also choose 'prctile')
-    'win_baseline': 60.,  # window for maximin
-    'sig_baseline': 10.,  # smoothing constant for gaussian filter
-    'prctile_baseline': 8.,  # optional (whether to use a percentile baseline)
-    'neucoeff': 0.7,  # neuropil coefficient
-    
+    'spikedetect': False, 
     # custom settings
     # 'remove_artifacts': (100, 512-100)
 }
